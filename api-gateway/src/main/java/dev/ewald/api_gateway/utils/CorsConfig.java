f9f4fd613159f4771b3d9f5801d61ee4d3da395f
Ewald B <ewald95@mail.ru>
2025-04-17 21:32:11 +0200

    feat(API-Gateway): Add cors configuration

---
+++ api-gateway/src/main/java/dev/ewald/api_gateway/utils/CorsConfig.java
@@ -0,0 +1,27 @@
+package dev.ewald.api_gateway.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig extends CorsConfiguration
+{
+    @Bean
+    public CorsWebFilter corsFilter()
+    {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration( "/**", config );
+
+        return new CorsWebFilter( source );
+    }
+}
