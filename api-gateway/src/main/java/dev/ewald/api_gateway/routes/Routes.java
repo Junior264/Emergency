18b376069f0c2197cbfc71b6fd4bf3d93c4ffe94
Ewald B <ewald95@mail.ru>
2026-02-25 14:07:28 +0100

    feat(API-Gateway): Refactor route paths

--- api-gateway/src/main/java/dev/ewald/api_gateway/routes/Routes.java
+++ api-gateway/src/main/java/dev/ewald/api_gateway/routes/Routes.java
@@ -5,54 +5,32 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 import dev.ewald.api_gateway.filter.AuthFilter;
-import dev.ewald.api_gateway.filter.RequestFilter;
-import dev.ewald.api_gateway.model.User;
 
 @Configuration
 public class Routes {
-    @Autowired
-    RequestFilter requestFilter;
-
     @Autowired
     AuthFilter authFilter;
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-            // .route("userdetails_service", r -> r.path("/register")
-            //     .and().method("POST")
-            //     .and().readBody(User.class, user -> true).filters(f -> f.filters(requestFilter, authFilter)
-            //     .uri("http://localhost:8080"))
-            .route("first-microservice",r -> r.path("/first")
-                .and().method("POST")
-                .and().readBody(User.class, user -> true).filters(filter -> filter.filters(requestFilter, authFilter))
-                .uri("http://localhost:8080"))
+            .route("userdetails_service", r -> r
+                .path("/register", "/login")
+                .uri("lb://USERDETAILS"))
+            .route("userdetails_service", r -> r
+                .path("/delete/**")
+                .filters(f -> f.filter(authFilter))
+                .uri("lb://USERDETAILS"))
+            .route("userdata_service", r -> r
+                .path("/notifications")
+                .filters(f -> f.filter(authFilter))
+                .uri("lb://USERDATA"))
+            .route("userdata_emergency", r -> r
+                .path("/notifications/emergency")
+                .filters(f -> f.filter(authFilter))
+                .uri("lb://USERDATA"))
             .build();    
-            // .route("first-microservice",r -> r.path("/first")
-            //     .and().method("POST")
-            //     .and().readBody(Student.class, s -> true).filters(f -> f.filters(requestFilter, authFilter))
-            //     .uri("http://localhost:8081"))
-            // .route("first-microservice",r -> r.path("/first")
-            //     .and().method("GET").filters(f-> f.filters(authFilter))
-            //     .uri("http://localhost:8081"))
-            // .route("second-microservice",r -> r.path("/second")
-            //     .and().method("POST")
-            //     .and().readBody(Company.class, s -> true).filters(f -> f.filters(requestFilter, authFilter))
-            //     .uri("http://localhost:8082"))
-            // .route("second-microservice",r -> r.path("/second")
-            //     .and().method("GET").filters(f-> f.filters(authFilter))
-            //     .uri("http://localhost:8082"))
-            // .route("auth-server",r -> r.path("/login")
-            //     .uri("http://localhost:8088"))
-            // .build();
-           
-    }
-
-    @Bean
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
     }
 }
