23c204163c146cc5c774eb511af86bd08cb90fd4
Ewald B <ewald95@mail.ru>
2026-02-25 14:05:45 +0100

    feat(API-Gateway): Add util class for jwt

--- api-gateway/src/main/java/dev/ewald/api_gateway/utils/JWTUtil.java
+++ api-gateway/src/main/java/dev/ewald/api_gateway/utils/JWTUtil.java
@@ -2,12 +2,12 @@ package dev.ewald.api_gateway.utils;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 import javax.crypto.SecretKey;
@@ -18,8 +18,7 @@ public class JWTUtil {
     @Value("${jwt.secret}")
     private String secret;
 
-    public Claims getALlClaims(String token) {
-        // return Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(token).getBody();
+    public Claims getAllClaims(String token) {
         return Jwts
             .parser()
             .verifyWith(getKey())
@@ -29,12 +28,11 @@ public class JWTUtil {
     }
 
     private SecretKey getKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secret);
-        return Keys.hmacShaKeyFor(keyBytes);
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
 
     private boolean isTokenExpired(String token ) {
-        return this.getALlClaims(token).getExpiration().before(new Date());
+        return this.getAllClaims(token).getExpiration().before(new Date());
     }
 
     public boolean isInvalid(String token) {
